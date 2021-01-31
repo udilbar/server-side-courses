a39ddcd9601426c5b4abe6d2cfc2bfb1c2aba7af
Ndn1618 <ndilbar1618@gmail.com>
2021-01-31 16:50:35 +0500

    Add index.html file and render it on the home router

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const CONFIG = require('./src/config')
 const app = express()
 
 app.use(express.static('src/assets'))
+app.set('views', 'src/views')
 
 app.engine('html', ejs.renderFile)
+app.set('view engine', 'html')
+
+app.get('/', (_, res) => {
+  res.render('index.html')
+})
 
 app.listen(CONFIG.PORT, () => console.log(CONFIG.PORT))
