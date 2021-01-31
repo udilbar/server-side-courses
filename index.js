43bbfad05fa6388001572a710ac6d7e6bd0b2462
Ndn1618 <ndilbar1618@gmail.com>
2021-01-31 16:19:02 +0500

    Set view engine as html

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const ejs = require('ejs')
 
 const CONFIG = require('./src/config')
 
 const app = express()
 
 app.use(express.static('src/assets'))
+
+app.engine('html', ejs.renderFile)
 
 app.listen(CONFIG.PORT, () => console.log(CONFIG.PORT))
