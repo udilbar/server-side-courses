958c4a6f9838d6de19767d86d9c5c76f640b2fea
Ndn1618 <ndilbar1618@gmail.com>
2021-02-01 10:07:43 +0500

    Create access token and send it when user has logged in

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const ejs = require('ejs')
+const sha1 = require('sha1')
+const { sign, verify } = require('./src/controllers/jwt')
 
 const CONFIG = require('./src/config')
 const users = require('./src/models/users')
@@ -9,10 +11,29 @@ const app = express()
 app.use(express.static('src/assets'))
 app.set('views', 'src/views')
 
-app.use(express.urlencoded());
+app.use(express.urlencoded())
+app.use(express.json())
 
 app.engine('html', ejs.renderFile)
 app.set('view engine', 'html')
+
+app.use(async (req, res, next) => {
+
+	if (req.url !== '/courses') {
+		next()
+	}
+	else {
+		try {
+      console.log(req.headers)
+			await verify(req.headers.inputToken)
+
+			next()
+		}
+		catch(error) {
+			res.status(401).send({ error: error })
+		}
+	}
+})
 
 app.get('/', (_, res) => {
   res.render('index.html')
@@ -41,13 +62,41 @@ app.get('/login', (_, res) => {
 app.post('/login', (req, res) => {
   const { userEmail, userPassword } = req.body
 
-  let user = users.find(user => user.email === userEmail && user.password === userPassword)
+  let user = users.find(user => user.email === userEmail && user.password === sha1(userPassword))
 
-  if (user) {
-    res.send('Succesfully logged in!');
-  } else {
-    res.status(401).end()
-  }
+  // if (user) {
+  //   res.send('Succesfully logged in!');
+  // } else {
+  //   res.status(401).end()
+  // }
+
+  // if (userEmail && userPassword) {
+
+	// 	const login = (user) => {
+
+	// 		return (
+	// 			user.username === userEmail &&
+	// 			user.password === sha1(userPassword)
+	// 		)
+	// 	}
+
+		// const user = users.find(login)
+
+		if (user) {
+
+			const accessToken = sign({
+				id: user.id,
+				role: user.role,
+			})
+
+			res.render('login-courses.html', {
+				accessToken: accessToken
+			})
+		}
+		else {
+			res.status(401).end()
+		}
+	// }
 })
 
 app.listen(CONFIG.PORT, () => console.log(CONFIG.PORT))
--- src/models/users.js
+++ src/models/users.js
@@ -3,7 +3,8 @@ const users = [
     id: 1,
     username: 'Ndn',
     email: "ndn@gmail.com",
-    password: 'ndn'
+    password: '9678d9292db91205c642f8ff46820edab0b92664',
+    role: 0
   }
 ]
 
