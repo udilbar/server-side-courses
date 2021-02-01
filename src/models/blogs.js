60b3a95e452c180ec82cdee517ec8c19045ad021
Ndn1618 <ndilbar1618@gmail.com>
2021-02-02 00:40:40 +0500

    Add blogs data

---
+++ src/models/blogs.js
@@ -0,0 +1,28 @@
+const blogs = [
+  {
+    id: 1,
+    title: "Technology",
+    text: "Pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    imageURL: "images/img9.png"
+  },
+  {
+    id: 2,
+    title: "Education",
+    text: "Pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    imageURL: "images/img8.png"
+  },
+  {
+    id: 3,
+    title: "Technology",
+    text: "Pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    imageURL: "images/img9.png"
+  },
+  {
+    id: 4,
+    title: "Education",
+    text: "Pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    imageURL: "images/img8.png"
+  }
+]
+
+module.exports = blogs
